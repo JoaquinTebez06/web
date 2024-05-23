d7c10919bfbfa6171059f72adca855053d5eab58
JoaquinTebez06 <157768090+JoaquinTebez06@users.noreply.github.com>
2024-05-22 22:07:41 -0300

    Add files via upload

---
+++ Pitagoras/public/script.js
@@ -0,0 +1,17 @@
+
+
+let hideText_btn = document.getElementById('hideText_btn')
+
+
+let moreText = document.getElementById('hideText');
+
+hideText_btn.addEventListener('click', toggleText);
+
+function toggleText(){
+    hideText.classList.toggle('show');
+
+    if(hideText.classList.contains('show')){hideText_btn.innerHTML = 'Leer menos';}
+    else{
+        hideText_btn.innerHTML = 'Leer más';
+    }
+}
